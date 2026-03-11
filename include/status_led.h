62e86dbdf9d99e766ec4933e172ecce545f07f2a
sakxam-xeetri <gbastakoti8@gmail.com>
2026-03-12 00:08:19 +0545

    add BLINK_NOTIFY pattern and notify method to StatusLED class

--- include/status_led.h
+++ include/status_led.h
@@ -19,15 +19,18 @@ public:
     // Call from FreeRTOS task loop
     void update();
 
-    enum Pattern { OFF, SOLID_ON, BLINK_SLOW, BLINK_FAST, BREATHE };
+    enum Pattern { OFF, SOLID_ON, BLINK_SLOW, BLINK_FAST, BREATHE, BLINK_NOTIFY };
     void setPattern(Pattern p);
+    void notify();  // quick triple-blink, then resume previous pattern
 
 private:
     Pattern _pattern = OFF;
+    Pattern _prevPattern = OFF;
     unsigned long _lastToggle = 0;
     bool _ledState = false;
     uint8_t _breatheVal = 0;
     int8_t  _breatheDir = 1;
+    uint8_t _notifyCount = 0;
 };
 
 extern StatusLED Led;
